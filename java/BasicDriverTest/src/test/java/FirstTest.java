142df4632c373d9d379da87ea347a674bb7ca950
Tom <tomtoster1@gmail.com>
2018-08-20 18:40:22 +0200

    JS checker screenshots WIP

--- java/BasicDriverTest/src/test/java/FirstTest.java
+++ java/BasicDriverTest/src/test/java/FirstTest.java
@@ -1,14 +1,25 @@
 package cineworld;
 
 import net.lightbody.bmp.core.har.HarEntry;
+
+import org.apache.commons.io.FileUtils;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Proxy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Predicate;
 
 import net.lightbody.bmp.BrowserMobProxyServer;
 import net.lightbody.bmp.client.ClientUtil;
@@ -26,7 +37,7 @@ public class FirstTest {
     public static final String PageURL="https://www.cineworld.co.uk/";
     public static final String PosterXPath="//div[@class='tab-pane collapsed active']//div[@aria-expanded='true']/div[@class='poster-container']";
 
-    @BeforeClass // Runs this method before the first test method in the current class is invoked
+    @BeforeClass
     public void setUp() {
         server = new BrowserMobProxyServer();
         server.start();
@@ -41,6 +52,7 @@ public class FirstTest {
     @Test
     public void gotoPage() {
         driver.get(PageURL);
+        waitForLoad(driver);
         driver.findElement(By.xpath("//section[@class='quickbook-component']"));
         driver.findElement(By.xpath("//button[@data-automation-id='searchByCinema']")).isEnabled();
         driver.findElement(By.xpath("//button[@data-automation-id='searchByFilm']")).isEnabled();
@@ -51,16 +63,23 @@ public class FirstTest {
     @Test
     public void expandMovieList() {
         driver.get(PageURL);
+        waitForLoad(driver);
         int before= driver.findElements(By.xpath(PosterXPath)).size();
+        captureScreen("before");
+        //File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        //FileUtils.copyFile(scrFile, new File("/scr_before.png"));
         driver.findElement(By.xpath("//img[@alt='Show more films']")).click();
         int after= driver.findElements(By.xpath(PosterXPath)).size();
+        captureScreen("after");
+        //scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        //FileUtils.copyFile(scrFile, new File("/scr_after.png"));
         Assert.assertTrue(after > before);
     }
 
     @Test
     public void comingSoonList() {
         driver.get(PageURL);
-        driver.findElement(By.xpath("//a[@id='comingSoon_tab']")).click();
+        waitForLoad(driver);
         int posters = driver.findElements(By.xpath(PosterXPath)).size();
         Assert.assertTrue(posters > 0);
     }
@@ -81,5 +100,28 @@ public class FirstTest {
         server.stop();
         driver.quit();
     }
+
+    public void waitForLoad(WebDriver driver) {
+        ExpectedCondition<Boolean> pageLoadCondition = new
+                ExpectedCondition<Boolean>() {
+                    public Boolean apply(WebDriver driver) {
+                        return ((JavascriptExecutor)driver).executeScript("return document.readyState").equals("complete");
+                    }
+                };
+        WebDriverWait wait = new WebDriverWait(driver, 30);
+        wait.until(pageLoadCondition);
+    }
+
+    public void captureScreen(String name) {
+        //String path;
+        try {
+            File source = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+            FileUtils.copyFile(source, new File(name+".png"));
+        }
+        catch(IOException e) {
+            System.out.println("Failed to capture screenshot: " + e.getMessage());
+        }
+    }
+
 
 }
